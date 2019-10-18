1f89b0989d0b4fb94ef19689257d2f4e056a0339
Ashley Beckett <ashley.m.beckett@gmail.com>
2019-10-18 03:28:02 +0000

    Saying hello to the world; unsure why it did not commit

--- hello.js
+++ hello.js
@@ -3,3 +3,14 @@ const sayHello  = function (name) {
 }
 
 sayHello("Ashley");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('Ashley # 2'); 
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
